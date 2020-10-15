a43cdd1c2e31a95fd6d71d64550b990601133187
Mihriban-O <mihriban.ozsoy34@gmail.com>
2020-10-15 23:14:40 +0300

    draft api file

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const Api = {
+  getMovies: () => {
+    return `getMovies() response: ${new Date()}`;
+  },
+  getSingleMovie: () => {
+    return `getSingleMovie() response: ${new Date()}`;
+  },
+};
+
+export default Api;
